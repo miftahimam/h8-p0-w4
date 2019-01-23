1003fff32739d48d53c7dec1c793e4661007fde1
miftahimam <44222082+miftahimam@users.noreply.github.com>
2019-01-24 01:44:44 +0700

    Update exercise-5.js

--- exercise-5.js
+++ exercise-5.js
@@ -1,11 +1,14 @@
 function ubahHuruf(kata) {
-    var output=[];
     var kamus = 'abcdefghijklmnopqrstuvwxyz'
-    
+    var output=''
+    //jika z, kembali ke a
  for (var i = 0 ; i< kata.length ;i+=1 ){
      for(var j = 0; j < kamus.length; j++){
         if(kamus[j] === kata[i]){
             output = output + kamus[j+1]
+        }else if(kamus[kamus.length-1] === kata[i]){
+            output += kamus[0]
+            break
         }
      
      }
@@ -14,14 +17,10 @@ function ubahHuruf(kata) {
  return output;
 
  
-	// for(var i = 0; i<kata.length; i++){
-    //       output+=String.fromCharCode(kata.charCodeAt(i) + 
-    //       (kata[i] === 'z' || kata[i] === 'Z' ? -25 : 1));
-    //   }
-//       return output;
 }
 
 // TEST CASES
+console.log(ubahHuruf('bisa azzzaa deh kamu')); // 
 console.log(ubahHuruf('wow')); // xpx
 console.log(ubahHuruf('developer')); // efwfmpqfs
 console.log(ubahHuruf('javascript')); // kbwbtdsjqu
